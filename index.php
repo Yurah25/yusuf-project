2a368f5e1e4aef5c2779dfd0e275673e9b3e2932
yurah25 <yusufrafiiahmad25@gmail.com>
2025-05-04 11:03:22 +0700

    tambah file index.php untuk menampilkan nama

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$nama= "Yusuf";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman awal</title>
+</head>
+<body>
+    <h1>Halo nama saya <?php echo $nama ?> </h1>
+</body>
+</html>
